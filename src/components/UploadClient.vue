a939ebef2deaca0af39d9b87d4b4ea51a0a7fc42
miracool <makurseme@gmail.com>
2023-07-06 22:50:50 +0100

    fear: added an upload function

--- src/components/UploadClient.vue
+++ src/components/UploadClient.vue
@@ -6,10 +6,45 @@
          class="imageInp" 
          id="upload"
          type="file" 
-         @change="onUpload" 
+         @change="onUpload"
          accept="image/*">
    </div>
 </template>
+
+<script>
+
+import cloudinaryConfig from '../../cloudinary';
+
+export default {
+   name: 'UploadClient',
+   data(){
+      return {
+        file: {},
+      }
+   },
+   methods: {
+      onUpload(e){
+         if(e.target.files[0].type === "image/*" &&  e.target.files[0].size  <= 2097152  ) {
+           this.addFile(e.target.files[0]);
+         }
+      },
+      addFile(value){
+         console.log('hello');
+         const file = value;
+         console.log(file);
+         cloudinaryConfig.upload(file).then(res => {
+            console.log('Image uploaded', res);
+         }).catch(err => {
+            console.log('an error occured:' + err);
+         })
+      }
+   }
+
+}
+
+</script>
+
+
 <style lang="css" scoped>
    .imageInp {
       display: none;
