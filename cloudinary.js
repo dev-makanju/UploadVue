18bc2785313fa562376db35e280c464e4f24767b
miracool <makurseme@gmail.com>
2023-07-06 06:48:58 +0100

    feat: set up cloudinary config

---
+++ cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from 'cloudinary-core';
+
+const cloudinaryConfig = cloudinary.Cloudinary.new({
+   cloud_name: 'dyz1ogpac',
+   api_key: '543724789842573',
+   api_secret: 'XboWnU-YhJ5x5ydeTNpyGVnN_eQ',
+})
+
+export default cloudinaryConfig;
